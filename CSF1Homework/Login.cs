33f62495a338ee9b29c1b3131c5800a8ae09d31f
Blaine Gomez <BlaineCGomez@outlook.com>
2020-10-03 21:07:01 -0500

    Worked on login.cs, still needs some work

---
+++ CSF1Homework/Login.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class Login
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            
+                bool userNameWrong = true;
+                int loopMax = 3;
+                int loopTimes = 1;
+
+
+            while (userNameWrong && loopTimes <= loopMax)
+            {
+                Console.WriteLine("Please enter your username: ");
+                string userName = Console.ReadLine().ToLower();
+                    if (userName == "purpleguy")
+                    {
+                        Console.WriteLine("Welcome, you have been granted access. ");
+                        userNameWrong = false;
+                    }//end if
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Sorry, you have been denied access. Try Again. ");
+                        loopTimes++;
+                    if (loopTimes == 4)
+                    {
+                        Console.WriteLine("You have tried too many times. You have been locked out.");
+                    }
+                    }//end else
+                }//end while
+                    
+        }//end main()
+    }//end class
+}//end mainspace
